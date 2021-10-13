24549e17f13271e745e9669228558ecb2f9979e7
Lourenço Gomes <leto.lg@gmail.com>
2021-10-13 10:49:27 +0100

    - Onclick Operation

--- app/src/main/java/ipca/example/calculator/MainActivity.kt
+++ app/src/main/java/ipca/example/calculator/MainActivity.kt
@@ -6,10 +6,20 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity(),View.OnClickListener {
+enum class Operator {
+    add,
+    divide,
+    subtract,
+    multiply
+}
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     lateinit var textViewDisplay : TextView
     var userIsInTheMiddleOfIntroduction = false
+
+    var operator : Operator? = null
+    var operand = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +37,12 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         val button0 = findViewById<Button>(R.id.button0)
         val buttonDot = findViewById<Button>(R.id.buttonDot)
         val buttonAC = findViewById<Button>(R.id.buttonAC)
+        val buttonPlus     = findViewById<Button>(R.id.buttonPlus    )
+        val buttonMinus    = findViewById<Button>(R.id.buttonMinus   )
+        val buttonMultiply = findViewById<Button>(R.id.buttonMultiply)
+        val buttonDivide   = findViewById<Button>(R.id.buttonDivide  )
+        val buttonEqual    = findViewById<Button>(R.id.buttonEqual   )
+
 
         textViewDisplay = findViewById<TextView>(R.id.textViewDisplay)
         button1.setOnClickListener {
@@ -45,19 +61,85 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         button0.setOnClickListener(this)
         buttonDot.setOnClickListener(this)
 
+        buttonPlus.setOnClickListener(doOperation)
+        buttonMinus.setOnClickListener(doOperation)
+        buttonMultiply.setOnClickListener(doOperation)
+        buttonDivide.setOnClickListener(doOperation)
+        buttonEqual.setOnClickListener {
+            userIsInTheMiddleOfIntroduction = false
+            performOperation()
+        }
+
         buttonAC.setOnClickListener{
             textViewDisplay.text = "0"
             userIsInTheMiddleOfIntroduction = false
         }
     }
 
+    fun performOperation() {
+        operator?.let { op ->
+            when(op) {
+                Operator.add -> {
+                    operand += textViewDisplay.text.toString().toDouble()
+                }
+                Operator.subtract -> {
+
+                }
+                Operator.multiply -> {
+
+                }
+                Operator.divide -> {
+
+                }
+            }
+        }
+
+
+
+
+        if (operand.rem(1)==0.0) {
+            textViewDisplay.text = operand.toInt().toString()
+        }else {
+            textViewDisplay.text = operand.toString()
+        }
+
+
+    }
+
+    val doOperation = object : View.OnClickListener{
+        override fun onClick(view: View?) {
+            val buttonPressed = view as Button
+            operator = when (buttonPressed.text) {
+                "+" ->  Operator.add
+                "-" ->  Operator.subtract
+                "*" ->  Operator.multiply
+                "/" ->  Operator.divide
+                else -> null
+            }
+            operand = textViewDisplay.text.toString().toDouble()
+            userIsInTheMiddleOfIntroduction = false
+        }
+
+    }
+
+
+
+
     override fun onClick(view: View?) {
         val buttonPressed = view as Button
         if (userIsInTheMiddleOfIntroduction){
-            textViewDisplay.text = "${textViewDisplay.text}${buttonPressed.text}"
-
+            if(!buttonPressed.text.equals(".")){
+                textViewDisplay.text = "${textViewDisplay.text}${buttonPressed.text}"
+            }else {
+                if (!textViewDisplay.text.contains(".")){
+                    textViewDisplay.text = "${textViewDisplay.text}${buttonPressed.text}"
+                }
+            }
         }else{
             textViewDisplay.text = buttonPressed.text
+            if(buttonPressed.text.equals(".")){
+                textViewDisplay.text = "0."
+            }
             userIsInTheMiddleOfIntroduction = true
         }
 
